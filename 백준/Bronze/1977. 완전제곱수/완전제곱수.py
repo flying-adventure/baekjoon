43111912e7f6d1e2658922eb1e45d6f0ee844f3c
뚭둡스 <99644015+flying-adventure@users.noreply.github.com>
2023-08-25 20:06:57 +0900

    [Bronze II] Title: 완전제곱수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.py
@@ -0,0 +1,17 @@
+# M < N
+M = int(input())
+N = int(input())
+
+sum = 0 
+min = 0 
+for i in range(1,101):
+    if M <= i*i and N >= i*i: 
+        if sum == 0:
+            min = i*i
+        sum += i*i
+if sum == 0 :
+    print(-1)
+else: 
+    print(sum)
+    print(min)
+    
