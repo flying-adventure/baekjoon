fffa2f55226329dd42a6144cbbee83c2b75106a1
뚭둡스 <99644015+flying-adventure@users.noreply.github.com>
2024-01-03 00:56:45 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main(void) {
+    printf("         ,r'\"7\n"); // 작은 따옴표, 큰 따옴표 출력을 위해서는 앞에 '\'를 추가 
+    printf("r`-_   ,\'  ,/\n");
+    printf(" \\. \". L_r\'\n"); // '\' 출력을 위해서는 '\\' 입력
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+
+}
