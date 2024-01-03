8de01fc615c598bd27723b53a9c06f42b26f1fb5
뚭둡스 <99644015+flying-adventure@users.noreply.github.com>
2024-01-03 14:53:55 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main(void){
+    char S[100];
+    scanf("%s", S);
+ 
+    int position[26];
+    for(int i=0; i<26; i++){
+        position[i] = -1;
+    }
+ 
+    for(int j=0; j<strlen(S); j++){
+        char alphabet = S[j];
+
+ 
+        if(position[alphabet-'a'] == -1)
+            position[alphabet-'a'] = j;
+        else
+            continue;
+    }
+ 
+    for(int k=0; k<26; k++){
+        printf("%d ", position[k]);
+    }
+}
