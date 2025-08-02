e6eb15b4391d090d4e6f12aeb16b098bde2753fe
안수빈 <99644015+flying-adventure@users.noreply.github.com>
2025-08-02 17:13:41 +0900

    Update FizzBuzz.c

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.c
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.c
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+// FizzBuzz 규칙에 따라 숫자 i의 출력 문자열을 구함
 void getFizzBuzz(int i, char* out) {
     if (i % 3 == 0 && i % 5 == 0)
         strcpy(out, "FizzBuzz");
@@ -11,29 +12,27 @@ void getFizzBuzz(int i, char* out) {
     else if (i % 5 == 0)
         strcpy(out, "Buzz");
     else
-        sprintf(out, "%d", i);
-}
-
-int is_number(const char* s) {
-    for (int i = 0; s[i]; i++) {
-        if (!isdigit(s[i])) return 0;
-    }
-    return 1;
+        sprintf(out, "%d", i);  // 숫자를 문자열로 변환
 }
 
 int main(void) {
+    //입력받기
     char a[10], b[10], c[10];
     scanf("%s %s %s", a, b, c);
 
     int base = -1;
 
-    if (is_number(a)) base = atoi(a) + 3;
-    else if (is_number(b)) base = atoi(b) + 2;
-    else if (is_number(c)) base = atoi(c) + 1;
+    // 입력 중 숫자인 줄을 찾고, 해당 위치에 따라 다음 출력 위치 계산
+    if (isdigit(a[0])) base = atoi(a) + 3;
+    else if (isdigit(b[0])) base = atoi(b) + 2;
+    else if (isdigit(c[0])) base = atoi(c) + 1;
 
+    // 다음 출력 결과 구하기
     char result[10];
     getFizzBuzz(base, result);
-    printf("%s", result);
+
+    // 결과 출력
+    printf("%s\n", result);
 
     return 0;
 }
