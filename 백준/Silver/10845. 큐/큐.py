5ef17dde2e02f3b2c8d788716b8e36dce1b60834
안수빈 <99644015+flying-adventure@users.noreply.github.com>
2026-02-18 16:01:02 +0900

    [Silver IV] Title: 큐, Time: 460 ms, Memory: 35056 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,28 @@
+from collections import deque
+N=int(input())
+queue=deque()
+for _ in range(N):
+    order=input().split()
+    if order[0]=="push":
+        queue.append(int(order[1]))
+    elif order[0]=="pop":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif order[0]=="size":
+        print(len(queue))
+    elif order[0]=="empty":
+        print(0 if queue else 1)
+    elif order[0]=="front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif order[0]=="back":
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
+    
+        
