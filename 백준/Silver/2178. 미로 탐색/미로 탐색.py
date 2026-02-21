7582b94634edb8b234c69c2adfe6663df0e7e6d6
안수빈 <99644015+flying-adventure@users.noreply.github.com>
2026-02-21 13:25:41 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -6,18 +6,21 @@ input=sys.stdin.readline
 N,M=map(int,input().split())
 board=[list(map(int,input().strip())) for _ in range(N)]
 
+##시작칸 설정
 dist=[[0]*M for _ in range(N)]
 dist[0][0]=1 
 q=deque([(0,0)])
 dirs=[(1,0),(-1,0),(0,1),(0,-1)]
 
+#순회: 큐에서 하나를 뺌, for문에 방향 고려하고 새로운 변수에 넣기, if board 범위, if 갈수있고and방문안한곳
+#방문횟수 증가시키고 큐에 좌표 넣기
 while q:
     x,y=q.popleft()
-    
     for dx,dy in dirs:
         nx,ny=x+dx,y+dy
         if 0<=nx<N and 0<=ny<M:
             if board[nx][ny]==1 and dist[nx][ny] ==0:
                 dist[nx][ny]=dist[x][y]+1
                 q.append((nx,ny))
+# 좌표 0부터 시작하는거 항상 고려하기               
 print(dist[N-1][M-1])
