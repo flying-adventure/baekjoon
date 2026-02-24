da8a10ccc8ddd9b93a097e4b57bb1cef364e628f
안수빈 <99644015+flying-adventure@users.noreply.github.com>
2026-02-24 21:14:01 +0900

    [level 2] Title: 구명보트, Time: 8.93 ms, Memory: 9.66 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,13 @@
+def solution(people, limit):
+    people.sort()
+    left = 0
+    right = len(people) - 1
+    ans = 0
+
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+        right -= 1
+        ans += 1
+
+    return ans
