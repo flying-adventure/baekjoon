330fa7cfa8d8fa2cfe9326393632941118f4c5cf
안수빈 <99644015+flying-adventure@users.noreply.github.com>
2026-02-25 11:19:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.09 ms, Memory: 25.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,10 @@
+from collections import deque
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for a,b in zip(participant,completion):
+        if a!=b:
+            return a
+    return participant[-1]
+        
+    
