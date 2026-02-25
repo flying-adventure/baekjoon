6906c18f6472c3f4d1201228f48ae20dfd05978b
안수빈 <99644015+flying-adventure@users.noreply.github.com>
2026-02-25 13:15:54 +0900

    [level 2] Title: 전화번호 목록, Time: 96.71 ms, Memory: 29.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,9 @@
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        a=phone_book[i]
+        b=phone_book[i+1]
+        if b.startswith(a):
+            return False
+    return True
+        
