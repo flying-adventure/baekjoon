12491204f0a66f3b64a8fb383c87d23e24a82122
안수빈 <99644015+flying-adventure@users.noreply.github.com>
2026-03-03 22:18:16 +0900

    [Silver II] Title: 최소 힙, Time: 108 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,17 @@
+#최소힙 : 부모노드가 제일 작음
+#배열에 자연수 x를 넣는다
+# 배열에서 가장 작은 값을 출력하고 그 값을 제거한다.
+import sys
+import heapq
+input=sys.stdin.readline
+N=int(input())
+heap=[]
+for _ in range(N):
+    x=int(input())
+    if x>0:
+        heapq.heappush(heap,x)
+    else:
+        if not heap:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
