4fc271b035cfdddac5d5b3f9ca57577c279b13b1
안수빈 <99644015+flying-adventure@users.noreply.github.com>
2026-03-19 21:02:40 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 1504 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.c
+++ 백준/Silver/10828. 스택/스택.c
@@ -1,97 +1,81 @@
-#include <stdio.h>
-#include<stdlib.h>
+#include<stdio.h>
 #include<string.h>
-#define INF 99999999
 
-typedef struct Node {
-    int data;
-    struct Node* next;
-}Node;
+int number[100001];
+int cnt = -1;
 
-typedef struct Stack {
-    Node* top;
-}Stack;
-//push : pushes an integer onto the stack.
-void push(Stack* stack, int data) {
-    Node* node = (Node*)malloc(sizeof(Node));
 
-    node->data = data;
-    node->next = stack->top;
-    stack->top = node;
-}
-//pop: Subtracts the top integer from the stack and prints the number. If there are no integers on the stack, -1 is printed.
-int pop(Stack* stack) {
-    if (stack->top == NULL) {
-        printf("-1\n");
-        return -INF;
-    }
-    printf("%d\n", stack->top->data);
-    Node* node = stack->top;
-    int data = node->data;
-    stack->top = node->next;
-    free(node);
-    return data;
-}
-//size: Outputs the number of integers on the stack.
-void size(Stack* stack) {
-    Node* now = stack->top;
-    int Size = 0;
-    while (now != NULL) {
-        Size++;
-        now = now->next;
-    }
-    printf("%d\n", Size);
-
-}
 //empty: Print 1 if the stack is empty, or 0 otherwise.
-int empty(Stack* stack) {
-    if (stack->top == NULL) {
-        printf("1\n");
-        return -INF;
-    }
-    else {
-        printf("0\n");
-    }
-
+int empty() {
+	if (cnt == -1) {
+		return 1;
+	}
+	else { return 0; }
 }
 
-//Top: Print the integer at the top of the stack. If there are no integers on the stack, print -1
-void Top(Stack* stack) {
-    if (stack->top == NULL) {
-        printf("-1\n");
-    }
-    else { printf("%d\n", stack->top->data); }
-
+//push X: pushes an integer X onto the stack.
+int push_X(int num) {
+	cnt++;
+	number[cnt] = num;
 }
 
-int main() {
-    Stack s;
-    s.top = NULL;
+//pop: Subtracts the top integer from the stack and prints the number. If there are no integers on the stack, -1 is printed.
+int pop() {
+	int k = empty();
+	if (k == 1) { printf("-1\n"); }
+	else {
+		printf("%d\n", number[cnt]);
+		number[cnt] = 0;
+		cnt--;
+	}
+}
 
-    int n;
-    char order[10];
-    scanf("%d", &n);
-    for (int k = 0; k < n; k++) {
-        scanf("%s", order);
-        if (!strcmp(order, "push")) {
-            int num;
-            scanf("%d", &num);
-            push(&s, num);
-        }
-        if (!strcmp(order, "pop")) {
-            pop(&s);
-        }
-        if (!strcmp(order, "size")) {
-            size(&s);
-        }
-        if (!strcmp(order, "empty")) {
-            empty(&s);
-        }
-        if (!strcmp(order, "top")) {
-            Top(&s);
-        }
+//size: Outputs the number of integers on the stack.
+int size() {
+	if (cnt == -1) {
+		printf("0\n");
+	}
+	else {
+		printf("%d\n", cnt + 1);
+	}
+}
 
-    }
+//top: Print the integer at the top of the stack. If there are no integers on the stack, print -1
+int top() {
+	int k = empty();
+	if (k == 1) {
+		printf("-1\n");
+	}
+	else {
+		printf("%d\n", number[cnt]);
+	}
 
-    return 0;
+}
+int main(void) {
+	int n;
+	char order[10];
+	scanf("%d", &n);
+	for (int k = 0; k < n; k++) {
+
+		scanf("%s", order);
+		if (!strcmp(order, "push")) {
+			int num;
+			scanf("%d", &num);
+			push_X(num);
+		}
+		if (!strcmp(order, "pop")) {
+			pop();
+		}
+		if (!strcmp(order, "size")) {
+			size();
+		}
+		if (!strcmp(order, "empty")) {
+			int p = empty();
+			printf("%d\n", p);
+		}
+		if (!strcmp(order, "top")) {
+			top();
+		}
+	}
+
 }
